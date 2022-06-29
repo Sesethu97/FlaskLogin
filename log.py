c223b0b3f7a594996dd6c45c3e6d3b9a761e8978
sluzipo021 <sluzipo021@student.wethinkcode.co.za>
2022-06-29 19:33:51 +0200

    "updated sign in"

--- log.py
+++ log.py
@@ -1,29 +1,35 @@
-
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 import pickle
 
 
+
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "abcd"
 
 
-@app.route('/')
+database = {"Sauce": "123", "Apple": "pin", "Pie": "okayokay"}
+
+
+
+@app.route("/")
 def hello_world():
     return render_template("login.html")
-database={'Sauce':'123','Apple':'pin','Pie':'okayokay'}
 
-@app.route('/form_login',methods=['POST','GET'])
+
+
+@app.route("/", methods=["POST", "GET"])
 def login():
-    name1=request.form['username']
-    pwd=request.form['password']
-     
-    
-    if name1 not in database:
-        return render_template('logihn.html', info='Invalid User')
-    else: 
-        if database[name1]!=pwd:
-            return render_template('login.html',info='Invalid Password')
-        else:
-	        return render_template('home.html',name=name1)
+    name1 = request.form["username"]
+    pwd = request.form["password"]
 
-if __name__ == '__main__':
+    if name1 not in database:
+        return render_template("login.html", info="Invalid User")
+    else:
+        if database[name1] != pwd:
+            return render_template("login.html", info="Invalid Password")
+        else:
+            return render_template("home.html", name=name1)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
